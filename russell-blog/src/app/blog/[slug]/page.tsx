a119aed54c9098122d08fcda3798c2cad67431b2
Russell Bradley <me@russellbradley.com>
2025-03-21 08:04:23 -0400

    more broke stuff

--- russell-blog/src/app/blog/[slug]/page.tsx
+++ russell-blog/src/app/blog/[slug]/page.tsx
@@ -4,12 +4,9 @@ import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
 
-export default async function BlogPostPage({
-  params,
-}: {
-  params: Awaited<{ slug: string }>;
-}) {
-  const { slug } = params;
+export default async function BlogPostPage(props: any) {
+  const { slug } = await Promise.resolve(props.params);
+
   const filePath = path.join("posts", `${slug}.md`);
   const fileContent = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContent);
